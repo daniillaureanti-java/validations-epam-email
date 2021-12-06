01a3bc7b504854831895e0460083b8304db57635
Daniil Laureanti <daniil.laureanti@gmail.com>
2021-12-06 11:15:38 +0200

    Completed task

--- src/main/java/com/epam/rd/autotasks/validations/EpamEmailValidation.java
+++ src/main/java/com/epam/rd/autotasks/validations/EpamEmailValidation.java
@@ -7,6 +7,12 @@ public class EpamEmailValidation {
 
     public static boolean validateEpamEmail(String email) {
         // Put your code here
+        if (email == null) {
+            return false;
+        }
+        final String regex = "^[a-zA-Z]+_[a-zA-Z]+[0-9]*@epam.com$";
+
+        return Pattern.matches(regex,email);
 
     }
 }
